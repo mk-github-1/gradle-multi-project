4b42b012a07ec6beb731fbc8e0e70285f91c18ed
mk-github-1 <masaya.kamii@gmail.com>
2023-05-11 05:22:53 +0900

    修正

--- user-interface/src/main/resources/data.sql
+++ user-interface/src/main/resources/data.sql
@@ -5,8 +5,6 @@ DELETE FROM public.user_role;
 DELETE FROM public.login_user;
 DELETE FROM public.role;
 
-DELETE FROM public.person;
-
 --------------------------------------------------
 
 -- role --
