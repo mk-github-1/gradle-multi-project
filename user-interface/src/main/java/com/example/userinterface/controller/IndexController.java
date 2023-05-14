29bf792c050fceb86506ad6cb98a3aaef1df1a01
mk-github-1 <32920703+mk-github-1@users.noreply.github.com>
2023-05-15 08:17:55 +0900

    Update IndexController.java

--- user-interface/src/main/java/com/example/userinterface/controller/IndexController.java
+++ user-interface/src/main/java/com/example/userinterface/controller/IndexController.java
@@ -19,4 +19,9 @@ public class IndexController {
     public String logout() {
         return "logout";
     }
+    
+    @GetMapping("/error")
+    public String error() {
+        return "error";
+    }
 }
